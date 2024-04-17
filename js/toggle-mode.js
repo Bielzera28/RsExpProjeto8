3d103ce2c9036352e827ed40df2b9ee4a64d83d6
Gabriel Aguiar Queiroz <108091647+Bielzera28@users.noreply.github.com>
2024-04-16 23:15:27 -0300

    Atualizações styles para toggle

--- js/toggle-mode.js
+++ js/toggle-mode.js
@@ -2,7 +2,3 @@ const buttonForest = document.getElementById("toggle-forest")
 const buttonRain = document.getElementById("toggle-rain")
 const buttonStore = document.getElementById("toggle-store")
 const buttonFire = document.getElementById("toggle-fire")
-
-buttonToggle.addEventListener("click", (event) => {
-  
-})
